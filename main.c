870c7db0021273bde6b0fcbae677d68e579f5376
DESKTOP-22CH0LN\andra <andraz.leitgeb@adora-med.com>
2017-08-22 09:37:02 +0200

    successful build of libcurl ... simple example working

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 ﻿#include <stdio.h>
 
 #include <curl.h>
+#include <easy.h>
 
 int main()
 {
-    printf("Hello World!\n");
+    CURL *curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+        /* example.com is redirected, so we tell libcurl to follow redirection */
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        /* Perform the request, res will get the return code */
+        res = curl_easy_perform(curl);
+        /* Check for errors */
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
     return 0;
 }
