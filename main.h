0e44cef7d6f1ad3cd1cf93d1e61c615a8bfa3d69
DESKTOP-22CH0LN\andra <andraz.leitgeb@adora-med.com>
2017-08-23 08:20:19 +0200

    data tests

---
+++ main.h
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <curl.h>
+#include "cJSON.h"
+
+/* Used by some code below as an example datatype. */
+struct record
+{
+    const char *precision;
+    double lat;
+    double lon;
+    const char *address;
+    const char *city;
+    const char *state;
+    const char *zip;
+    const char *country;
+};
+
+int main();
+
+static void create_objects(void);
+static int print_preallocated(cJSON *root);
+
+static int sum(int a);
+
